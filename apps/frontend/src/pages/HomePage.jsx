92350ce4a802de68c84dbc98d050f57f719f4991
Nayanika1402 <132455412+Nayanika1402@users.noreply.github.com>
2025-05-29 09:56:42 +0530

    Home page added

---
+++ apps/frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+// pages/HomePage.js
+import React from 'react';
+import HeroSection from '../components/HeroSection';
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroSection />
+      {/* Add more sections here: features, testimonials, etc. */}
+    </div>
+  );
+};
+
+export default HomePage;
