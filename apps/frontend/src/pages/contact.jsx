1d70767dd024cc8e46bf0acf12858da0dec2e643
503BINGPOT <dhruv503a@gmail.com>
2025-08-23 08:19:46 +0530

    improved contact section by adding message sent successfull text after submitting

--- apps/frontend/src/pages/contact.jsx
+++ apps/frontend/src/pages/contact.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import '../index.css';
 import contactImage from '../assets/images/contactimage.png' ;
+import { useState,useRef } from "react"
 
 const contact = () => {
+
+  const [loading, setLoading] = useState(false);
+  const form = useRef()
+  const [success ,setSuccess] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+      setSuccess(true);
+
+      setTimeout(() => setSuccess(false), 3000);
+    }, 2000);
+  };
+
   return (
     <section id='contact' className="contact-section">
       <div className="contact-card">
@@ -12,10 +30,15 @@ const contact = () => {
         <div className="contact-form">
             <h2>Get in touch</h2>
             <p>Have any questions, feedback, or suggestions? We'd love to hear from you! <br/> Fill out the form below and our team will get back to you as soon as possible.</p>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type='email' placeholder='Enter a Valid Email adress' required></input>
                 <input type='text' placeholder='Enter your Name' required></input>
-                <button type='submit'>Submit</button>
+                <button
+                type="submit"
+                disabled={loading}>
+                {loading ? 'Sending...' : 'Send Message'}
+                </button>
+                {success && <p>Message sent Successfully!</p>}
             </form>
         </div>
 
